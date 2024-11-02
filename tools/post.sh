3a43dc0e633925dab5be5fc2232b0b151d93c5ae
knowgyu <gyuheon.noh@gmail.com>
2024-11-02 16:43:25 +0000

    OS Start

--- tools/post.sh
+++ tools/post.sh
@@ -8,8 +8,8 @@ author: knowgyu
 description: " "
 date: $(date +%Y-%m-%d) $(date +%H:%M:%S) +0900
 math: true
-categories: [MLOps, Kubeflow-Pipeline-YOLOv8]
-tags: [MLOps, Kubeflow]
+categories: [Embedded System, 임베디드 OS 개발 프로젝트]
+tags: [Embedded System, RTOS, Firmware, OS]
 ---
 
 여기에 글 내용을 작성하세요.
