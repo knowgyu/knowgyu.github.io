2874cc33b0b263aa21d507c88f5c5b0e8f877d5e
knowgyu <gyuheon.noh@gmail.com>
2024-11-04 15:09:15 +0000

    posting shell update

--- tools/algo_post.sh
+++ tools/algo_post.sh
@@ -12,24 +12,7 @@ categories: [Problem Solving, BOJ]
 tags: [Problem Solving, Algorithm]
 ---
 
-![image.png](/assets/img/OS/OS000.jpg){: .w-25}
-<br>
-
-**본 프로젝트는 이만우 저자님의 "임베디드 OS 개발 프로젝트" 교재를 따라 RTOS를 만드는 것을 목표로 합니다.**
-
-<br>
-
-***
-
-[여기에 글 내용을 작성하세요.]
-
-
-## 참고
-***
-
-참고 깃허브 : [https://github.com/navilera/Navilos](https://github.com/navilera/Navilos)
-
-이만우 저자님의 블로그 주소 : [https://kldp.org/node/162560](https://kldp.org/node/162560)
+여기에 글 내용을 작성하세요.
 
 EOM
 echo "New post created: $filename"
--- tools/os_post.sh
+++ tools/os_post.sh
@@ -12,6 +12,24 @@ categories: [Embedded System, 임베디드 OS 개발 프로젝트]
 tags: [Embedded System, RTOS, Firmware, OS]
 ---
 
-여기에 글 내용을 작성하세요.
+![image.png](/assets/img/OS/OS000.jpg){: .w-25}
+<br>
+
+**본 프로젝트는 이만우 저자님의 "임베디드 OS 개발 프로젝트" 교재를 따라 RTOS를 만드는 것을 목표로 합니다.**
+
+<br>
+
+***
+
+[여기에 글 내용을 작성하세요.]
+
+
+## 참고
+***
+
+참고 깃허브 : [https://github.com/navilera/Navilos](https://github.com/navilera/Navilos)
+
+이만우 저자님의 블로그 주소 : [https://kldp.org/node/162560](https://kldp.org/node/162560)
+
 EOM
 echo "New post created: $filename"
