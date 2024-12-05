cec18be27a88411e9ed55ca3898751497dcbdde7
knowgyu <gyuheon.noh@gmail.com>
2024-12-05 14:29:03 +0000

    tracking pjt

---
+++ tools/basic_post.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+read -p "Post title: " title
+filename="_posts/$(date +%Y-%m-%d)-${title// /-}.md"
+cat >$filename <<-EOM
+---
+title: "$title"
+author: knowgyu
+description: " "
+date: $(date +%Y-%m-%d) $(date +%H:%M:%S) +0900
+math: true
+categories: []
+tags: []
+---
+
+여기에 글 내용을 작성하세요.
+
+EOM
+echo "New post created: $filename"
