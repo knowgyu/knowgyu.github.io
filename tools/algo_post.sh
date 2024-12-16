1d5fe69295b11fbea4148bcec2d71e1c3c47f7b7
knowgyu <gyuheon.noh@gmail.com>
2024-12-16 16:02:02 +0000

    Upload BOJ 20541

--- tools/algo_post.sh
+++ tools/algo_post.sh
@@ -8,8 +8,8 @@ author: knowgyu
 description: " "
 date: $(date +%Y-%m-%d) $(date +%H:%M:%S) +0900
 math: true
-categories: [Computer Science, 코딩테스트 합격자되기 C++편]
-tags: [Algorithm, DataStructure]
+categories: [Computer Science, Problem Solving]
+tags: [Algorithm, Problem Solving, BOJ]
 ---
 
 여기에 글 내용을 작성하세요.
