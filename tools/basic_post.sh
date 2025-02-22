c721ebdcff44c8731d86faf6a46bc8b2d0764f86
knowgyu <gyuheon.noh@gmail.com>
2025-02-22 17:02:08 +0000

    Upload ros start

--- tools/basic_post.sh
+++ tools/basic_post.sh
@@ -8,8 +8,8 @@ author: knowgyu
 description: " "
 date: $(date +%Y-%m-%d) $(date +%H:%M:%S) +0900
 math: true
-categories: []
-tags: []
+categories: [Embedded System, ROS]
+tags: [ROS, 자율주행, SLAM]
 ---
 
 여기에 글 내용을 작성하세요.
