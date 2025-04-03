5ed3042fb90d151dd2e6113a1ce1412631fb680e
knowgyu <gyuheon.noh@gmail.com>
2025-04-03 00:47:21 +0000

    업데이트: nvm 업데이트 node npm 설치

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,7 +1,28 @@
 #!/usr/bin/env bash
 
+# 패키지 존재 여부에 관계없이 항상 NVM과 Node.js LTS 설치
+echo "Installing NVM and Node.js LTS..."
+
+# NVM 경로 설정
+export NVM_DIR="/usr/local/share/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+# NVM 로드 확인
+command -v nvm
+
+nvm install --lts
+nvm use --lts
+node --version
+npm --version
+
+# Netlify CLI 전역 설치
+echo "Installing Netlify CLI..."
+npm install -g netlify-cli
+netlify --version
+
+# 기존 package.json 관련 로직도 유지
 if [ -f package.json ]; then
-  bash -i -c "nvm install --lts && nvm install-latest-npm"
+  echo "Found package.json, installing dependencies..."
   npm i
   npm run build
 fi
@@ -16,3 +37,6 @@ sed -i -E "s/^(plugins=\()(git)(\))/\1\2 zsh-syntax-highlighting zsh-autosuggest
 
 # Avoid git log use less
 echo -e "\nunset LESS" >>~/.zshrc
+
+# NVM 자동 로드 설정 추가
+echo -e "\n# NVM 자동 로드\nexport NVM_DIR=\"\$HOME/.nvm\"\n[ -s \"\$NVM_DIR/nvm.sh\" ] && . \"\$NVM_DIR/nvm.sh\"" >>~/.zshrc
